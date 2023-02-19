d5bb70dea20891537479f636ecb1c34982d87c34
Benu <88966166+Benhur-me@users.noreply.github.com>
2023-02-20 01:16:51 +0300

    My First Vue code

---
+++ pull from github/Intro-to-Vue-3/main.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        premium: true,
+        cart: []
+      }
+    },
+    methods: {
+      updateCart(id) {
+        this.cart.push(id)
+      },
+      
+    },
+    
+  })
